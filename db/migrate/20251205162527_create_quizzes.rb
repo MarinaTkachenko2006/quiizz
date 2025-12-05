b5ef07a1d5e1de4dd9d23e78606e6b3e65d55ba6
MarinaTkachenko2006 <tkachenkomari2006@gmai.com>
2025-12-05 16:28:30 +0000

    create db

---
+++ db/migrate/20251205162527_create_quizzes.rb
@@ -0,0 +1,29 @@
+class CreateQuizzes < ActiveRecord::Migration[8.1]
+  def change
+    create_table :quizzes do |t|
+      t.string :title, null: false
+      t.string :description
+      t.integer :author_id, null: false
+      t.boolean :is_public, null: false
+      t.timestamps
+      t.index :title, unique: true
+    end
+
+    create_table :questions do |t|
+      t.integer :quiz_id, null: false
+      t.string :text, null: false
+      t.integer :time_limit, null: false
+      t.integer :reward, null: false
+      t.integer :order_index, null: false
+      t.timestamps
+    end
+
+    create_table :answers do |t|
+      t.integer :question_id, null: false
+      t.string :answer_text, null: false
+      t.boolean :is_correct, null: false
+      t.timestamps
+    end
+
+  end
+end
