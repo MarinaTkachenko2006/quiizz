e67b7bb4ca3cb80d103850b48711719f36878600
CyanCactus4 <depcoffmel@gmail.com>
2025-12-09 04:11:06 +0300

    The second design update

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -9,7 +9,8 @@ class QuizzesController < ApplicationController
 
   def show
     @quiz = Quiz.find(params[:id])
-  @questions = @quiz.questions.includes(:answers).order(:order_index)
+    @questions = @quiz.questions.includes(:answers).order(:order_index)
+    
   end
 
   def new
