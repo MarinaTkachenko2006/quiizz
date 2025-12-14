4ee2471550fd233a66dd5a68072a2083539365fd
CyanCactus4 <depcoffmel@gmail.com>
2025-12-14 11:40:18 +0300

    Added the new table - CreateQuizzSession

---
+++ db/migrate/20251214083834_create_quiz_sessions.rb
@@ -0,0 +1,13 @@
+class CreateQuizSessions < ActiveRecord::Migration[8.1]
+  def change
+    create_table :quiz_sessions do |t|
+      t.references :quiz, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.integer :score, default 0
+      t.integer :number_correct_answers, default 0
+      t.boolean :is_completed, default: false
+
+      t.timestamps
+    end
+  end
+end
